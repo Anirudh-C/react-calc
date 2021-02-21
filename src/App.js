3743bd0fb4b06354e22513d48279fd0931e03ce1
Anirudh C <anirudh.c@iiitb.org>
2021-02-22 01:20:29 +0530

    add: dark-theme and calculator outline card

--- src/App.js
+++ src/App.js
@@ -1,34 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import { makeStyles } from '@material-ui/core/styles';
 
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from "@material-ui/core/Switch";
 import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    root: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        minWidth: 400,
+        minHeight: 400,
+    },
+    title: {
+        fontSize: 14,
+    },
 }));
 
 export default function App() {
-  const classes = useStyles();
+    const classes = useStyles();
+    const [darkState, setDarkState] = useState(false);
 
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Typography component="h1" variant="h4">
-          Scientific Calculator
-        </Typography>
-        <Button variant="contained" color="primary">
-          Press for Hurray!
-        </Button>
-      </div>
-    </Container>
-  );
+    const darkTheme = createMuiTheme({
+        palette: {
+            type: "dark",
+            primary: {
+                main: "#6272a4"
+            },
+            secondary: {
+                main: "#bd93f9"
+            }
+        }
+    });
+
+    return (
+        <ThemeProvider theme={darkTheme}>
+          <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+              <Typography component="h1" variant="h4">
+                Scientific Calculator
+              </Typography>
+              <CssBaseline />
+              <Card className={classes.root} variant="outlined">
+              </Card>
+            </div>
+          </Container>
+        </ThemeProvider>
+    );
 }
