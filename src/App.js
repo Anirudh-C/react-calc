5dd7ae3c720d7d20ccd7892675a806282fefb0cc
Anirudh C <anirudh.c@iiitb.org>
2021-03-04 18:48:23 +0530

    fix: json formatting standard

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Layout from './Layout.js';
 import buttonCallback from './callbacks.js';
 
+const gridLabels = require('./labels.json');
+
 class App extends React.Component {
     render() {
-        const gridLabels = require('./labels.json');
         return (
             <Layout grid={gridLabels} callback={buttonCallback}/>
         );
