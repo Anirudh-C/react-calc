68647d3408120d417ff9f67485716fcc84dbc95d
Anirudh C <anirudh.c@iiitb.org>
2021-03-05 22:35:17 +0530

    add: buttons render test

--- src/Layout.test.js
+++ src/Layout.test.js
@@ -14,6 +14,18 @@ describe('Layout', () => {
         const component = shallow(<Layout grid={gridLabels} callback={buttonCallback}/>);
     });
 
+    it('should render all butons defined in labels.json', () => {
+        const actualCount = gridLabels["normalGrid"]
+              .map((row) => row.elements.length)
+              .reduce((total, x) => (total + x), 0) +
+              gridLabels["scientificGrid"]
+              .map((row) => row.elements.length)
+              .reduce((total, x) => (total + x), 0);
+
+        const component = mount(<Layout grid={gridLabels} callback={buttonCallback}/>);
+        expect(component.find(Button).length).toBe(actualCount);
+    });
+
     it('should call the defined callback function', () => {
         const component = mount(<Layout grid={gridLabels} callback={mockCallback}/>);
 
