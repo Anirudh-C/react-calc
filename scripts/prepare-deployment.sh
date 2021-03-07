6de1c167afa381fb5ae8c9376b67362d13e2796b
Anirudh C <anirudh.c@iiitb.org>
2021-03-07 14:35:09 +0530

    update: logging service start and docker ignore

--- scripts/prepare-deployment.sh
+++ scripts/prepare-deployment.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+trap "kill 0" EXIT
 echo "Starting Jenkins...";
 systemctl start jenkins;
+echo "Starting logger...";
+npm run logger &
 echo "Starting Webhook Relay Agent...";
 relay forward --bucket github-jenkins http://localhost:8080/github-webhook/
-
-
